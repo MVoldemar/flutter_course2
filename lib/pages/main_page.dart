9e3480133757b2abe3e7f8aeea1e4e5fe78bef86
MVoldemar <65668419+MVoldemar@users.noreply.github.com>
2021-06-19 09:32:38 +0300

    10_generation

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fight_club/pages/statistics_page.dart';
 import 'package:flutter_fight_club/resources/fight_club_colors.dart';
-import 'package:flutter_fight_club/resources/fight_page.dart';
+import 'package:flutter_fight_club/pages/fight_page.dart';
 import 'package:flutter_fight_club/widgets/action_button.dart';
 import 'package:flutter_fight_club/widgets/secondary_action_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
